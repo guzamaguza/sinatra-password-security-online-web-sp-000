627094feaa145bc38661832d100cf7b200209820
Mark A Guza <guzamaguza@gmail.com>
2020-05-21 03:50:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,11 @@ class ApplicationController < Sinatra::Base
 
 	post "/signup" do
 		user = User.new(:username => params[:username], :password => params[:password])
+		if 
+			redirect to '/login'
+		else 
+			redirect to '/failure'
+		end
 	end
 
 	get "/login" do
